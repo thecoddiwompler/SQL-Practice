407686f79e92cad6d06faf344155f7bc74883473
Indrajeet Mishra <mindra673@gmail.com>
2023-06-19 15:21:50 +0530

    Create schema.sql

---
+++ Investments in 2016/schema.sql
@@ -0,0 +1,6 @@
+Create Table If Not Exists Insurance (pid int, tiv_2015 float, tiv_2016 float, lat float, lon float)
+Truncate table Insurance
+insert into Insurance (pid, tiv_2015, tiv_2016, lat, lon) values ('1', '10', '5', '10', '10')
+insert into Insurance (pid, tiv_2015, tiv_2016, lat, lon) values ('2', '20', '20', '20', '20')
+insert into Insurance (pid, tiv_2015, tiv_2016, lat, lon) values ('3', '10', '30', '20', '20')
+insert into Insurance (pid, tiv_2015, tiv_2016, lat, lon) values ('4', '10', '40', '40', '40')
