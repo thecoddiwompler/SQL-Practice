1308347a157737677f20f65d4d3650bef94a1718
Indrajeet Mishra <mindra673@gmail.com>
2023-06-20 11:38:32 +0530

    Create schema.sql

---
+++ Exchange Seats/schema.sql
@@ -0,0 +1,7 @@
+Create table If Not Exists Seat (id int, student varchar(255))
+Truncate table Seat
+insert into Seat (id, student) values ('1', 'Abbot')
+insert into Seat (id, student) values ('2', 'Doris')
+insert into Seat (id, student) values ('3', 'Emerson')
+insert into Seat (id, student) values ('4', 'Green')
+insert into Seat (id, student) values ('5', 'Jeames')
