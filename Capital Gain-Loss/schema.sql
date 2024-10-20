4f8dd3cdcb01c374453a654fa9360479e470dd7d
Indrajeet Mishra <mindra673@gmail.com>
2024-10-20 15:49:02 +0530

    corrected schema

--- Capital Gain-Loss/schema.sql
+++ Capital Gain-Loss/schema.sql
@@ -1,12 +1,22 @@
-Create Table If Not Exists Stocks (stock_name varchar(15), operation ENUM('Sell', 'Buy'), operation_day int, price int)
-Truncate table Stocks
-insert into Stocks (stock_name, operation, operation_day, price) values ('Leetcode', 'Buy', '1', '1000')
-insert into Stocks (stock_name, operation, operation_day, price) values ('Corona Masks', 'Buy', '2', '10')
-insert into Stocks (stock_name, operation, operation_day, price) values ('Leetcode', 'Sell', '5', '9000')
-insert into Stocks (stock_name, operation, operation_day, price) values ('Handbags', 'Buy', '17', '30000')
-insert into Stocks (stock_name, operation, operation_day, price) values ('Corona Masks', 'Sell', '3', '1010')
-insert into Stocks (stock_name, operation, operation_day, price) values ('Corona Masks', 'Buy', '4', '1000')
-insert into Stocks (stock_name, operation, operation_day, price) values ('Corona Masks', 'Sell', '5', '500')
-insert into Stocks (stock_name, operation, operation_day, price) values ('Corona Masks', 'Buy', '6', '1000')
-insert into Stocks (stock_name, operation, operation_day, price) values ('Handbags', 'Sell', '29', '7000')
-insert into Stocks (stock_name, operation, operation_day, price) values ('Corona Masks', 'Sell', '10', '10000')
+CREATE TABLE IF NOT EXISTS Stocks (
+    stock_name VARCHAR(15),
+    operation VARCHAR(4) CHECK (operation IN ('Sell', 'Buy')),
+    operation_day INT,
+    price INT
+);
+
+TRUNCATE TABLE Stocks;
+
+INSERT INTO
+    Stocks (stock_name, operation, operation_day, price)
+VALUES
+    ('Leetcode', 'Buy', '1', '1000'),
+    ('Corona Masks', 'Buy', '2', '10'),
+    ('Leetcode', 'Sell', '5', '9000'),
+    ('Handbags', 'Buy', '17', '30000'),
+    ('Corona Masks', 'Sell', '3', '1010'),
+    ('Corona Masks', 'Buy', '4', '1000'),
+    ('Corona Masks', 'Sell', '5', '500'),
+    ('Corona Masks', 'Buy', '6', '1000'),
+    ('Handbags', 'Sell', '29', '7000'),
+    ('Corona Masks', 'Sell', '10', '10000');
