7a4870e4b3e97b311425345391996c6873cd0800
Indrajeet Mishra <mindra673@gmail.com>
2024-10-21 18:36:03 +0530

    corrected schema

--- Department Top 3 Salary/schema.sql
+++ Department Top 3 Salary/schema.sql
@@ -1,13 +1,29 @@
-Create table If Not Exists Employee (id int, name varchar(255), salary int, departmentId int)
-Create table If Not Exists Department (id int, name varchar(255))
-Truncate table Employee
-insert into Employee (id, name, salary, departmentId) values ('1', 'Joe', '85000', '1')
-insert into Employee (id, name, salary, departmentId) values ('2', 'Henry', '80000', '2')
-insert into Employee (id, name, salary, departmentId) values ('3', 'Sam', '60000', '2')
-insert into Employee (id, name, salary, departmentId) values ('4', 'Max', '90000', '1')
-insert into Employee (id, name, salary, departmentId) values ('5', 'Janet', '69000', '1')
-insert into Employee (id, name, salary, departmentId) values ('6', 'Randy', '85000', '1')
-insert into Employee (id, name, salary, departmentId) values ('7', 'Will', '70000', '1')
-Truncate table Department
-insert into Department (id, name) values ('1', 'IT')
-insert into Department (id, name) values ('2', 'Sales')
+CREATE TABLE IF NOT EXISTS Employee (
+    id int,
+    name varchar(255),
+    salary int,
+    departmentId int
+);
+
+CREATE TABLE IF NOT EXISTS Department (id int, name varchar(255));
+
+TRUNCATE TABLE Employee;
+
+INSERT INTO
+    Employee (id, name, salary, departmentId)
+VALUES
+    (1, 'Joe', 85000, 1),
+    (2, 'Henry', 80000, 2),
+    (3, 'Sam', 60000, 2),
+    (4, 'Max', 90000, 1),
+    (5, 'Janet', 69000, 1),
+    (6, 'Randy', 85000, 1),
+    (7, 'Will', 70000, 1);
+
+TRUNCATE TABLE Department;
+
+INSERT INTO
+    Department (id, name)
+VALUES
+    (1, 'IT'),
+    (2, 'Sales');
