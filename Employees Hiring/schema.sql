64a60232f26f0618bc8a250d4590ea6fce0471b8
Indrajeet Mishra <mindra673@gmail.com>
2024-10-21 22:44:07 +0530

    corrected schema

--- Employees Hiring/schema.sql
+++ Employees Hiring/schema.sql
@@ -1,39 +1,53 @@
-Create table candidates(
-id int primary key,
-positions varchar(10) not null,
-salary int not null);
+CREATE SCHEMA employees;
+
+CREATE TABLE employees.candidates(
+    id int PRIMARY KEY,
+    positions varchar(10) NOT NULL,
+    salary int NOT NULL
+);
 
 --Test Case 1
-
 TRUNCATE TABLE employees.candidates;
-insert into employees.candidates values(1,'junior',5000);
-insert into employees.candidates values(2,'junior',7000);
-insert into employees.candidates values(3,'junior',7000);
-insert into employees.candidates values(4,'senior',10000);
-insert into employees.candidates values(5,'senior',30000);
-insert into employees.candidates values(6,'senior',20000);
+
+INSERT INTO
+    employees.candidates
+VALUES
+    (1, 'junior', 5000),
+    (2, 'junior', 7000),
+    (3, 'junior', 7000),
+    (4, 'senior', 10000),
+    (5, 'senior', 30000),
+    (6, 'senior', 20000);
 
 --Test Case 2
-
 TRUNCATE TABLE employees.candidates;
-insert into employees.candidates values(20,'junior',10000);
-insert into employees.candidates values(30,'senior',15000);
-insert into employees.candidates values(40,'senior',30000);
+
+INSERT INTO
+    employees.candidates
+VALUES
+    (20, 'junior', 10000),
+    (30, 'senior', 15000),
+    (40, 'senior', 30000);
 
 --Test Case 3
-
 TRUNCATE TABLE employees.candidates;
-insert into employees.employees.candidates values(1,'junior',15000);
-insert into employees.employees.candidates values(2,'junior',15000);
-insert into employees.employees.candidates values(3,'junior',20000);
-insert into employees.employees.candidates values(4,'senior',60000);
+
+INSERT INTO
+    employees.candidates
+VALUES
+    (1, 'junior', 15000),
+    (2, 'junior', 15000),
+    (3, 'junior', 20000),
+    (4, 'senior', 60000);
 
 --Test Case 4
-
 TRUNCATE TABLE employees.candidates;
-insert into employees.candidates values(10,'junior',10000);
-insert into employees.candidates values(40,'junior',10000);
-insert into employees.candidates values(20,'senior',15000);
-insert into employees.candidates values(30,'senior',30000);
-insert into employees.candidates values(50,'senior',15000);
 
+INSERT INTO
+    employees.candidates
+VALUES
+    (10, 'junior', 10000),
+    (40, 'junior', 10000),
+    (20, 'senior', 15000),
+    (30, 'senior', 30000),
+    (50, 'senior', 15000);
--- Exchange Seats/schema.sql
+++ Exchange Seats/schema.sql
@@ -1,7 +1,12 @@
-Create table If Not Exists Seat (id int, student varchar(255))
-Truncate table Seat
-insert into Seat (id, student) values ('1', 'Abbot')
-insert into Seat (id, student) values ('2', 'Doris')
-insert into Seat (id, student) values ('3', 'Emerson')
-insert into Seat (id, student) values ('4', 'Green')
-insert into Seat (id, student) values ('5', 'Jeames')
+CREATE TABLE IF NOT EXISTS Seat (id int, student varchar(255));
+
+TRUNCATE TABLE Seat;
+
+INSERT INTO
+    Seat (id, student)
+VALUES
+    (1, 'Abbot'),
+    (2, 'Doris'),
+    (3, 'Emerson'),
+    (4, 'Green'),
+    (5, 'Jeames');
