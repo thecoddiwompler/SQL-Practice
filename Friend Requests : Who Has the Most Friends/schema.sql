f9364bffa9f76db219573390c5e3809849f4c240
Indrajeet Mishra <mindra673@gmail.com>
2024-10-22 23:43:35 +0530

    corrected schema

--- Friend Requests : Who Has the Most Friends/schema.sql
+++ Friend Requests : Who Has the Most Friends/schema.sql
@@ -1,6 +1,15 @@
-Create table If Not Exists RequestAccepted (requester_id int not null, accepter_id int null, accept_date date null)
-Truncate table RequestAccepted
-insert into RequestAccepted (requester_id, accepter_id, accept_date) values ('1', '2', '2016/06/03')
-insert into RequestAccepted (requester_id, accepter_id, accept_date) values ('1', '3', '2016/06/08')
-insert into RequestAccepted (requester_id, accepter_id, accept_date) values ('2', '3', '2016/06/08')
-insert into RequestAccepted (requester_id, accepter_id, accept_date) values ('3', '4', '2016/06/09')
+CREATE TABLE IF NOT EXISTS RequestAccepted (
+    requester_id int NOT NULL,
+    accepter_id int NULL,
+    accept_date date NULL
+);
+
+TRUNCATE TABLE RequestAccepted;
+
+INSERT INTO
+    RequestAccepted (requester_id, accepter_id, accept_date)
+VALUES
+    (1, 2, '2016-06-03'),
+    (1, 3, '2016-06-08'),
+    (2, 3, '2016-06-08'),
+    (3, 4, '2016-06-09');
